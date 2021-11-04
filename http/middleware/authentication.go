c58487483c946361771ae9d159aa6ca82e443b7c
Kensuke Kubo <kensukekubo19@gmail.com>
2021-11-04 10:25:54 +0900

    fix: use many session

--- http/middleware/authentication.go
+++ http/middleware/authentication.go
@@ -8,16 +8,20 @@ import (
 	jwt "github.com/ken109/gin-jwt"
 )
 
-func Authentication(realm string) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		if c.GetHeader("Authorization") == "" {
-			session := sessions.Default(c)
-			token := session.Get("token")
-			if token, ok := token.(string); ok {
-				c.Request.Header.Set("Authorization", fmt.Sprintf("bearer %s", token))
+func Authentication(jwtRealm string, session string) gin.HandlerFunc {
+	if session != "" {
+		return func(c *gin.Context) {
+			if c.GetHeader("Authorization") == "" {
+				session := sessions.DefaultMany(c, session)
+				token := session.Get("token")
+				if token, ok := token.(string); ok {
+					c.Request.Header.Set("Authorization", fmt.Sprintf("bearer %s", token))
+				}
 			}
-		}
 
-		jwt.Verify(realm)(c)
+			jwt.Verify(jwtRealm)(c)
+		}
+	} else {
+		return jwt.Verify(jwtRealm)
 	}
 }
